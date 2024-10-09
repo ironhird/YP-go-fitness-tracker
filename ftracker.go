027fc03144cac8e8d5e10d9c67affafdd95d75d5
Dmitrii Sosnin <ironhird@gmail.com>
2024-10-09 19:40:49 +0700

    First commit for review

--- ftracker.go
+++ ftracker.go
@@ -48,19 +48,19 @@ func ShowTrainingInfo(action int, trainingType string, duration, weight, height 
     // ваш код здесь
     switch {
 	case trainingType == "Бег":
-		distance := ... // вызовите здесь необходимую функцию
-		speed := ... // вызовите здесь необходимую функцию
-		calories := ... // вызовите здесь необходимую функцию
+		distance := distance(action) // вызовите здесь необходимую функцию
+		speed := meanSpeed(action, duration) // вызовите здесь необходимую функцию
+		calories := RunningSpentCalories(action, weight, duration)  // вызовите здесь необходимую функцию
 		return fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n", trainingType, duration, distance, speed, calories)
 	case trainingType == "Ходьба":
-		distance := ... // вызовите здесь необходимую функцию
-		speed := ... // вызовите здесь необходимую функцию
-		calories := ... // вызовите здесь необходимую функцию
+		distance := distance(action) // вызовите здесь необходимую функцию
+		speed := meanSpeed(action, duration) // вызовите здесь необходимую функцию
+		calories := WalkingSpentCalories(action, duration, weight, height) // вызовите здесь необходимую функцию
 		return fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n", trainingType, duration, distance, speed, calories)
 	case trainingType == "Плавание":
-		distance := ... // вызовите здесь необходимую функцию
-		speed := ... // вызовите здесь необходимую функцию
-		calories := ... // вызовите здесь необходимую функцию
+		distance := distance(action) // вызовите здесь необходимую функцию
+		speed := swimmingMeanSpeed(lengthPool, countPool, duration) // вызовите здесь необходимую функцию
+		calories := SwimmingSpentCalories(lengthPool, countPool, duration, weight) // вызовите здесь необходимую функцию
 		return fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n", trainingType, duration, distance, speed, calories)
 	default:
 		return "неизвестный тип тренировки"
@@ -82,7 +82,13 @@ const (
 // duration float64 — длительность тренировки в часах.
 func RunningSpentCalories(action int, weight, duration float64) float64 {
     // ваш код здесь
-    ...
+    // ((18 * СредняяСкоростьВКм/ч * 1.79) * ВесСпортсменаВКг / 
+    // mInKM  * ВремяТренировкиВЧасах * minInH)
+    var res float64
+    res = meanSpeed(action, duration)
+    res = res * runningCaloriesMeanSpeedMultiplier * runningCaloriesMeanSpeedShift * weight
+    res = res / (mInKm * duration * minInH)
+    return res
 }
 
 // Константы для расчета калорий, расходуемых при ходьбе.
@@ -101,7 +107,17 @@ const (
 // height float64 — рост пользователя.
 func WalkingSpentCalories(action int, duration, weight, height float64) float64 {
     // ваш код здесь
-    ...
+    /*  ( (0.035 * ВесСпортсменаВКг + 
+        (СредняяСкоростьВМетрахВСекунду**2 / РостВМетрах)
+         * 0.029 * ВесСпортсменаВКг)
+         * ВремяТренировкиВЧасах * minInH ) */
+    var res float64
+    res = meanSpeed(action, duration)
+    res = math.Pow(res, 2) / height
+    res = res * walkingSpeedHeightMultiplier * weight
+    res = res + walkingCaloriesWeightMultiplier * weight
+    res = res * duration * minInH
+    return res
 }
 
 // Константы для расчета калорий, расходуемых при плавании.
@@ -134,5 +150,9 @@ func swimmingMeanSpeed(lengthPool, countPool int, duration float64) float64 {
 // weight float64 — вес пользователя.
 func SwimmingSpentCalories(lengthPool, countPool int, duration, weight float64) float64 {
     // ваш код здесь
-    ...
+    // (СредняяСкоростьВКм/ч + 1.1) * 2 * ВесСпортсменаВКг * ВремяТренировкиВЧасах
+    var res float64
+    res = swimmingMeanSpeed(lengthPool, countPool, duration) + swimmingCaloriesMeanSpeedShift
+    res = res * swimmingCaloriesWeightMultiplier * weight * duration
+    return res
 }
